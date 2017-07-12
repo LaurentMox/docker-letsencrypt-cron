4aa438fd80e97522fee4be2c5f7f9b0a6fd5c9ab
thanhbv <thanhbv@sandinh.net>
2017-07-12 15:37:17 +0700

    fix run_certbot.sh

--- certbot/run_certbot.sh
+++ certbot/run_certbot.sh
@@ -1,8 +1,10 @@
 # see https://certbot.eff.org/docs/using.html#certbot-command-line-options
-echo "Running certbot with COMMON_ARGS='$COMMON_ARGS' and LOOP_ARGS='$LOOP_ARGS'"
 
 IFS='|'
 for args in $LOOP_ARGS; do
+echo "Running: certbot $COMMON_ARGS $args"
+unset IFS
 certbot $COMMON_ARGS $args
+IFS='|'
 done
 unset IFS
