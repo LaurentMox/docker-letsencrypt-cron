7ab02029bdc77729f1be8c79134c8b5b2a93a3d8
lmn <lmn@one2team.com>
2017-10-14 14:11:27 +0200

    Add deploy hook script

---
+++ certbot/deploy-hook-script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+my_dir="$(dirname "$0")"
+source "$my_dir/functions.sh"
+
+set -e
+
+for domain in $RENEWED_DOMAINS; do
+        cert_root="/certs/$domain/"
+
+        # Make sure the certificate and private key files are
+        # never world readable, even just for an instant while
+        # we're copying them into cert_root.
+        umask 077
+
+        cp "$RENEWED_LINEAGE/fullchain.pem" "$cert_root/fullchain.pem"
+        cp "$RENEWED_LINEAGE/privkey.pem" "$cert_root/privkey.pem"
+done
+
+docker_kill "$NGINX_PROXY_CONTAINER" SIGHUP
--- certbot/run_certbot.sh
+++ certbot/run_certbot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # see https://certbot.eff.org/docs/using.html#certbot-command-line-options
+
 my_dir="$(dirname "$0")"
-source "$my_dir/functions.sh"
+
 IFS='|'
 for args in $LOOP_ARGS; do
-echo "Running: certbot $COMMON_ARGS $args"
-unset IFS
-certbot $COMMON_ARGS $args
-IFS='|'
+    echo "Running: certbot $COMMON_ARGS $args & deploy-hook-script"
+    unset IFS
+    certbot --deploy-hook $my_dir/deploy-hook-script.sh $COMMON_ARGS $args
+    IFS='|'
 done
 unset IFS
-docker_kill "$NGINX_PROXY_CONTAINER" SIGHUP
